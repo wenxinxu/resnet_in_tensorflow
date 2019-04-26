cdae3b9e7945427afede3b9c4500c2ea5350df2c
Xiaokeai18 <im.wangpei@outlook.com>
2019-04-26 17:23:42 +0800

    mypr

--- hyper_parameters.py
+++ hyper_parameters.py
@@ -7,9 +7,9 @@ FLAGS = tf.app.flags.FLAGS
 
 ## The following flags are related to save paths, tensorboard outputs and screen outputs
 
-tf.app.flags.DEFINE_string('version', 'test_1', '''A version number defining the directory to save
+tf.app.flags.DEFINE_string('version', 'test_110', '''A version number defining the directory to save
 logs and checkpoints''')
-tf.app.flags.DEFINE_integer('report_freq', 500, '''Steps takes to output errors on the screen
+tf.app.flags.DEFINE_integer('report_freq', 391, '''Steps takes to output errors on the screen
 and write summaries''')
 tf.app.flags.DEFINE_float('train_ema_decay', 0.95, '''The decay factor of the train error's
 moving average shown on tensorboard''')
@@ -34,7 +34,7 @@ tf.app.flags.DEFINE_integer('decay_step1', 60000, '''At which step to decay the 
 
 ## The following flags define hyper-parameters modifying the training network
 
-tf.app.flags.DEFINE_integer('num_residual_blocks', 10, '''How many residual blocks do you want''')
+tf.app.flags.DEFINE_integer('num_residual_blocks', 5, '''How many residual blocks do you want''')
 tf.app.flags.DEFINE_float('weight_decay', 0.0002, '''scale for l2 regularization''')
 
 
@@ -51,7 +51,7 @@ directory to restore''')
 tf.app.flags.DEFINE_boolean('is_use_ckpt', False, '''Whether to load a checkpoint and continue
 training''')
 
-tf.app.flags.DEFINE_string('test_ckpt_path', './logs_test_1/model.ckpt-79999', '''Checkpoint
+tf.app.flags.DEFINE_string('test_ckpt_path', 'model_110.ckpt-79999', '''Checkpoint
 directory to restore''')
 
 
